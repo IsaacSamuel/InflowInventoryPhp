e04ac94c6ac45425891b537ca38704c07d807040
Isaac Samuel <getintouchwithisaac@gmail.com>
2020-08-13 17:55:52 -0400

    Set api header method added

--- src/inflowApi.php
+++ src/inflowApi.php
@@ -1,15 +1,34 @@
 <?php
 declare(strict_types=1);
-
 namespace InflowAPI;
+
+require __DIR__ . "/../vendor/autoload.php";
+
+use Httpful\Request;
+
 
 final class InflowAPI {
     private $secret_key;
     private $company_id;
+    private $api_version;
 
-    public function __construct(string $secret_key, string $company_id) {
+    public function __construct(string $secret_key, string $company_id, string $api_version = '2020-08-06') {
         $this->secret_key = $secret_key;
         $this->company_id = $company_id;
+        $this->api_version = $api_version;
+    }
+
+    // Initializes our request object with headers required by API (including secret API key)
+    private function init_api_request() {
+        $template = Request::init();
+
+        $template->addHeaders(array(
+            'Authorization' => 'Bearer ' . $this->secret_key,
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json;version=' . $this->api_version
+        ));
+
+        return $template;
     }
 }
 
--- tests/inflowApiTest.php
+++ tests/inflowApiTest.php
@@ -6,25 +6,69 @@ use InflowAPI\InflowAPI;
 use PHPUnit\Framework\TestCase;
 
 final class inflowApiTest extends TestCase {
+    // Utility functions
+    // Utility function for calling a private method
+    public function invokeMethod(&$object, $methodName, array $parameters = array()) {
+        $reflection = new \ReflectionClass(get_class($object));
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
 
-    public function testObjectCanBeCreatedWithInvalidNullKeys() {
+        return $method->invokeArgs($object, $parameters);
+    }
+
+    //Utility function for returning a inflow api object using secret keys from .env file
+    public function getInflowAPIObject() {
+        //Load secret keys from .env file
+        $dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
+        $dotenv->load();
+        $secret_key = getenv("SECRET_API_KEY");
+        $company_id = getenv("COMPANY_ID");
+        $api_version = getenv("API_VERSION");
+
+        //Api version is optional
+        if($api_version) {
+            $inflow_api = new InflowAPI($secret_key, $company_id, $api_version);
+        }
+        else {
+            $inflow_api = new InflowAPI($secret_key, $company_id);
+        }
+        return $inflow_api;
+    }
+
+    //TESTS
+    public function testObjectCannotBeCreatedWithNullKeys() {
         $this->expectException(TypeError::class);
         
         $inflow_api = new InflowAPI(null, null);
     }
 
     public function testObjectCanBeCreatedWithValidKeys() {
-        //Load secret keys from .env file
-        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-        $dotenv->load();
-        $secret_key = $_ENV["SECRET_API_KEY"];
-        $company_id = $_ENV["COMPANY_ID"];
-
-        $inflow_api = new InflowAPI($secret_key, $company_id);
+        $inflow_api = $this->getInflowAPIObject();
         
         $this->assertInstanceOf(
             InflowAPI::class,
             $inflow_api
+        );
+    }
+
+    public function testHeadersAreSetCorrectly() {
+        //Load secret keys from .env file
+        $dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
+        $dotenv->load();
+        $secret_key = getenv("SECRET_API_KEY");
+        $api_version = (getenv("API_VERSION") ? $_ENV["API_VERSION"] : '2020-08-06');
+        
+        $inflow_api = $this->getInflowAPIObject();
+
+        $request = $this->invokeMethod($inflow_api, 'init_api_request', array());
+
+        $this->assertEquals(
+            $request->headers,
+            array(
+                'Authorization' => 'Bearer ' . $secret_key,
+                'Content-Type' => 'application/json',
+                'Accept' => 'application/json;version=' . $api_version 
+            )
         );
     }
 }
